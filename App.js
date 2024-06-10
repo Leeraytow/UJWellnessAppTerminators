daf843b2b7160b98754cb3eb7496468d39ec1349
Ronica030402 <130221767+Ronica030402@users.noreply.github.com>
2024-06-10 10:18:21 +0200

    Update App.js
    added new page

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import EmailVerification from './src/Logins/EmailVerificationScreen';
 import RegEmailVerification from './src/Logins/RegEmailVerificationScreen';
 import TherapyType from './src/Therapy/Therapytype';
 import DigitalDiary from './src/Therapy/DiaryEntry';
+import EntryReceivedPage from './src/EntryReceived';
 
 const Stack = createStackNavigator();
 
@@ -36,7 +37,8 @@ export default function App() {
         <Stack.Screen name="RegEmailVerification" component={RegEmailVerification} options={{ headerShown: false }} />
         <Stack.Screen name="TherapyType" component={TherapyType} options={{ headerShown: false }} />
         <Stack.Screen name="DigitalDiary" component={DigitalDiary} options={{ headerShown: false }} />
-
+        <Stack.Screen name="EntryReceivedPage" component={EntryReceivedPage} options={{ headerShown: false }}  />
+        
       
 
       </Stack.Navigator>
