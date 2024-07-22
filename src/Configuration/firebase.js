c342ccb5e122e8259917ce02a06383714cb5df84
Edward <131533448+Brutal-X@users.noreply.github.com>
2024-07-22 23:05:39 +0200

    firebase.js

--- src/Configuration/firebase.js
+++ src/Configuration/firebase.js
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIwAIJEXRa38LgiJPUJth1wbTSw5hB0ZM",
-  authDomain: "ujwellness-5b386.firebaseapp.com",
-  projectId: "ujwellness-5b386",
-  storageBucket: "ujwellness-5b386.appspot.com",
-  messagingSenderId: "720562431915",
-  appId: "1:720562431915:web:8ce7d19d3e48dd20e614ee",
-  measurementId: "G-0GCRT1580X"
+  apiKey: "AIzaSyDsmNjBPkjHmJlZi0RTW9FE59DZCFkBoBo",
+  authDomain: "ujwellness-cb043.firebaseapp.com",
+  projectId: "ujwellness-cb043",
+  storageBucket: "ujwellness-cb043.appspot.com",
+  messagingSenderId: "33750787509",
+  appId: "1:33750787509:web:80f352a370f057a8c865fc"
 };
 
 // Initialize Firebase
