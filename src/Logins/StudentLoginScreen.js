12ed445edc3416a4c89fba2acd02f4612753df34
LindelaniXabaWilie <lindelanixaba07@gmail.com>
2024-10-07 09:33:30 +0200

    admin

--- src/Logins/StudentLoginScreen.js
+++ src/Logins/StudentLoginScreen.js
@@ -118,7 +118,7 @@ export default function StudentAuthScreen({ navigation }) {
       await setDoc(userRef, { active: true }, { merge: true });
   
       // Navigate based on email domain
-      if (Email === 'webdevelopers@gmail.com') {
+      if (Email === 'nhlanhlamsomi2024@gmail.com') {
         navigation.navigate('AdminHomeScreen');
       } else if (Email.endsWith('@gmail.com')) {
         navigation.navigate('TherapistLandingScreen');
